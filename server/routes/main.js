ba7e07fe6dd0bcb9cb45d0dba89df9f3582fd3f0
jordan Moldovan <jmoldyvan@gmail.com>
2022-09-30 20:58:41 -0700

    clean a lil code

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = express.Router()
 const authController = require('../controllers/auth') 
-const homeController = require('../controllers/home')
 const hotSpringController = require('../controllers/hotSpring')
 const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
