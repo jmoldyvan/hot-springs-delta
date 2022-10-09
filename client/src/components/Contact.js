55ffdedbeac809f7c989839cb4b45a5f9afc17d5
commander jordan <jmoldyvan@gail.com>
2022-10-09 12:16:29 -0700

    fixed contact email bug

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -15,7 +15,7 @@ export default function Contact (props) {
         email: email.value,
         message: message.value,
       };
-      let response = await fetch("http://localhost:5000/contact", {
+      let response = await fetch("https://west-coast-hot-springs-api-5czk.onrender.com/contact", {
         method: "POST",
         headers: {
           "Content-Type": "application/json;charset=utf-8",
